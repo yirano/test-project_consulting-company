799ca48ff276e4cbea7fc8d894899bc54ddfa331
yirano <yirano@protonmail.ch>
2020-12-17 18:17:03 -0500

    Add jumbo image and submenu links

--- src/Components/Header/CallToAction.jsx
+++ src/Components/Header/CallToAction.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import ImageConsulting from '../../Assets/man-consulting.jpeg'
 
 const CallToAction = () => {
     return (
         <div className="cta">
             <h1>Call to action</h1>
-            <img src={ImageConsulting} />
         </div>
     )
 }
--- src/Components/Header/SubMenu.jsx
+++ src/Components/Header/SubMenu.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 const SubMenu = () => {
     return (
         <div className="subMenu">
-            <h1>SubMenu</h1>
+            <div>
+                <p>How can we help?</p>
+            </div>
+            <div>
+                <a href="#">I own a business.</a>
+                <a href="#">I manage or work on a team.</a>
+                <a href="#">I''m here to learn.</a>
+            </div>
         </div>
     )
 }
