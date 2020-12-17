48b9ac5fd6dd4b13e8f87b43b1e034ad4a283105
yirano <yirano@protonmail.ch>
2020-12-17 16:54:02 -0500

    Push up to work from macbook

--- src/Components/Header/CallToAction.jsx
+++ src/Components/Header/CallToAction.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import ImageConsulting from '../../Assets/man-consulting.jpeg'
 
 const CallToAction = () => {
     return (
         <div className="cta">
             <h1>Call to action</h1>
+            <img src={ImageConsulting} />
         </div>
     )
 }
