27590ed48345245b1851afb5afc19e2d95a32490
yirano <yirano@protonmail.ch>
2020-12-17 15:26:48 -0500

    Init to win it

---
+++ src/Components/Header/CallToAction.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CallToAction = () => {
+    return (
+        <div className="cta">
+            <h1>Call to action</h1>
+        </div>
+    )
+}
+
+export default CallToAction
---
+++ src/Components/Header/SubMenu.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SubMenu = () => {
+    return (
+        <div className="subMenu">
+            <h1>SubMenu</h1>
+        </div>
+    )
+}
+
+export default SubMenu
