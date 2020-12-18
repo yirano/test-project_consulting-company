1351e84266bbfc18a517be6f37df01b0957dcd91
yirano <yirano@protonmail.ch>
2020-12-17 21:06:47 -0500

    Header has all elements -- incomplete

--- src/Components/Header/CallToAction.jsx
+++ src/Components/Header/CallToAction.jsx
@@ -3,7 +3,16 @@ import React from 'react'
 const CallToAction = () => {
     return (
         <div className="cta">
-            <h1>Call to action</h1>
+            <div className="ctaMessage">
+
+                <h1>Creating Clear Direction,</h1>
+                <h1>Intuitive Processes,</h1>
+                <h1>And Engaged Teams.</h1>
+            </div>
+            <div className="ctaLinks">
+                <button>Get to know us</button>
+                <p>Ready to get started? <span><a href="#">Contact us today.</a></span></p>
+            </div>
         </div>
     )
 }
