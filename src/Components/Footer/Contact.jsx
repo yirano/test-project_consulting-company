0f0d708af9634f02d1634ab5223c49ceaf48fbd8
yirano <yirano@protonmail.ch>
2020-12-18 18:23:59 -0500

    Contact in footer is styled and fully responsive

--- src/Components/Footer/Contact.jsx
+++ src/Components/Footer/Contact.jsx
@@ -7,15 +7,20 @@ const Contact = () => {
     return (
         <div className="contact">
             <div className="phoneEmail">
-                <h2>555.555.5555</h2>
+                <h1>555.555.5555</h1>
                 <p>Or send us an email using our <a href="#">contact form</a>.</p>
             </div>
-
+            <div className="spacer" />
             <div className="addressSocial">
                 <address>
-                    <h5>
-                        1234 Address, Springfield, MO 65806
-                    </h5>
+                    <div>
+                        <h4>
+                            1234 Address,
+                        </h4>
+                        <h4>
+                            Springfield, MO 65806
+                        </h4>
+                    </div>
                     <p>
                         <a href="#">Get directions</a> to our office.
                     </p>
