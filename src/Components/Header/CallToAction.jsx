0ff974c4c24b932ccf001b6c74a08a4a577958c4
yirano <yirano@protonmail.ch>
2020-12-19 11:59:23 -0500

    Header is fully responsive and styled -- burger menu incomplete

--- src/Components/Header/CallToAction.jsx
+++ src/Components/Header/CallToAction.jsx
@@ -4,14 +4,13 @@ const CallToAction = () => {
     return (
         <div className="cta">
             <div className="ctaMessage">
-
                 <h1>Creating Clear Direction,</h1>
                 <h1>Intuitive Processes,</h1>
                 <h1>And Engaged Teams.</h1>
             </div>
             <div className="ctaLinks">
                 <button>Get to know us</button>
-                <p>Ready to get started? <span><a href="#">Contact us today.</a></span></p>
+                <p>Ready to get started? <a href="#">Contact us today.</a></p>
             </div>
         </div>
     )
--- src/Components/Header/SubMenu.jsx
+++ src/Components/Header/SubMenu.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons'
 
 const SubMenu = () => {
     return (
         <div className="subMenu">
-            <div>
+            <div className="subMenuPrompt">
+                <FontAwesomeIcon icon={faUserCircle} size="3x" />
                 <p>How can we help?</p>
             </div>
             <div>
                 <a href="#">I own a business.</a>
                 <a href="#">I manage or work on a team.</a>
-                <a href="#">I''m here to learn.</a>
+                <a href="#">I'm here to learn.</a>
             </div>
         </div>
     )
