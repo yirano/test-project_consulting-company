116577b66b16610069ad68a5cae5b07cd35ee70f
yirano <yirano@protonmail.ch>
2020-12-19 12:15:41 -0500

    Submenu is fully responsive

--- src/Components/Header/SubMenu.jsx
+++ src/Components/Header/SubMenu.jsx
@@ -9,7 +9,7 @@ const SubMenu = () => {
                 <FontAwesomeIcon icon={faUserCircle} size="3x" />
                 <p>How can we help?</p>
             </div>
-            <div>
+            <div className="subMenuLinks">
                 <a href="#">I own a business.</a>
                 <a href="#">I manage or work on a team.</a>
                 <a href="#">I'm here to learn.</a>
