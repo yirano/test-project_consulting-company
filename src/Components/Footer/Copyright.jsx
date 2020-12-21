724707dd6f103dff48d72a1afb5cfd96b7e90e07
yirano <yirano@protonmail.ch>
2020-12-20 22:24:06 -0500

    Copyright is being rendered from mock API

--- src/Components/Footer/Copyright.jsx
+++ src/Components/Footer/Copyright.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
 
-const Copyright = () => {
+const Copyright = (props) => {
+    const { copyright } = props
+    console.log(copyright)
     return (
         <div className="copyright">
             <p>
-                &copy; Pretend Company Consulting Group. All Rights Reserved
+                {copyright.symbol} {copyright.text}
             </p>
-            <p><a href="#">Privacy Policy</a></p>
-            <p><a href="#">Terms &amp; Conditions</a></p>
-            <p><a href="#">Careers</a></p>
-            <p>Website crafted by <a href="#">Mostly Serious</a></p>
-
+            {copyright.link.map(l => <p>{l.text} <a href={l.url}>{l.link_text}</a></p>)}
         </div>
     )
 }
